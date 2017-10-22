2504a86769a84adeeea760c96274e6129279bf8c
littlehera <villanuevahera@gmail.com>
2017-10-22 20:34:40 +0800

    added tasks.py

---
+++ lotto_addons/tasks.py
@@ -0,0 +1,18 @@
+import frappe
+
+def renumbers_submit_so():
+    sos= frappe.db.sql("""SELECT name from `tabSales Order` ORDER BY name asc""")
+    for i, so in enumerate(sos):
+        id = "SO-"+str(i+1).zfill(5)
+        print "Count: " + str(i + 1) + "| ID: " + id + " | SO: " + so[0]
+        if id != so[0]:
+            print "RENAME!"
+            frappe.db.sql("""UPDATE `tabSales Order` set name = %s where name = %s""",(id, so[0]))
+            frappe.db.sql("""UPDATE `tabSales Order Item` set parent = %s where parent = %s""",(id, so[0]))
+            frappe.db.commit()
+            submit_doc("Sales Order", id)
+            #frappe.get_doc("Sales Order",id)
+
+def submit_doc(doctype, id):
+    doc = frappe.get_doc(doctype, id)
+    doc.submit()
