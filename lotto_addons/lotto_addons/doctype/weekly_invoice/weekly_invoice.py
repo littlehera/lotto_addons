85ba6847cae809c3e3471d897aaa5027583d777a
littlehera <villanuevahera@gmail.com>
2017-11-24 11:10:19 +0800

    Print Format Update: added footer

--- lotto_addons/lotto_addons/doctype/weekly_invoice/weekly_invoice.py
+++ lotto_addons/lotto_addons/doctype/weekly_invoice/weekly_invoice.py
@@ -32,11 +32,17 @@ def get_weekly_sales_orders(from_date, to_date, retailer):
 		doc = frappe.get_doc("Sales Order", so[0])
 		so_dict["so"] = doc.name
 		so_dict["date"] = doc.transaction_date
+		print "========+RETAILER=============="
+		print retailer
+		disc = frappe.get_doc("Customer", retailer).retailer_discount
+		#discount = retailer.discount
+		print disc
+
 		#Discount = (net sales / 1.16) x .09 or .07
 		so_dict["tpm"] = doc.pos_no
 		so_dict["sales"] = doc.net_sales
-		so_dict["discount"] = (doc.net_sales/1.16 * 0.07)
-		so_dict["sales_totals"] = (float(doc.net_sales) - (doc.net_sales/1.16 * 0.07))
+		so_dict["discount"] = (doc.net_sales/1.16) * (disc/100)
+		so_dict["sales_totals"] = (float(doc.net_sales) - (doc.net_sales/1.16 * disc))
 		so_dict["prizes"] = doc.payout
 		so_dict["prizes_discount"] = (doc.payout * 0.02)
 		so_dict["prizes_totals"]= -1*(float(doc.payout)+(doc.payout * 0.02))
